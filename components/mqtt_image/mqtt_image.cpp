ca9f17b6e68d8203f5a5de8eb7a5642de86069b2
TimPhillip <timphillip.schneider@t-online.de>
2024-12-01 20:10:47 +0100

    fixed wrong image type

--- components/mqtt_image/mqtt_image.cpp
+++ components/mqtt_image/mqtt_image.cpp
@@ -8,7 +8,7 @@ namespace mqtt_image{
 
 MQTTImage::MQTTImage(const uint8_t *data_start, int width, int height):
 image::Image(
-  data_start, width, height, image::ImageType::RGB_565 
+  data_start, width, height, image::ImageType::IMAGE_TYPE_RGB565
 ){
 
 }
