ce671abfd0315cb3250d8ec355c28a93748b26b0
TimPhillip <timphillip.schneider@t-online.de>
2024-12-01 20:06:48 +0100

    added namespace to source

--- components/mqtt_image/mqtt_image.cpp
+++ components/mqtt_image/mqtt_image.cpp
@@ -2,9 +2,16 @@
 #include "esphome/core/hal.h"
 
 
+namespace esphome{
+namespace mqtt_image{
+
+
 MQTTImage::MQTTImage(const uint8_t *data_start, int width, int height):
 image::Image(
   data_start, width, height, image::ImageType::RGB_565 
 ){
 
+}
+
+}
 }
