87e49eac8f96dba3c22b5ad411e2b75f859093e8
TimPhillip <timphillip.schneider@t-online.de>
2024-12-01 19:51:37 +0100

    added namespace

--- components/mqtt_image/mqtt_image.h
+++ components/mqtt_image/mqtt_image.h
@@ -7,9 +7,15 @@
 #include "esphome/components/lvgl/lvgl_proxy.h"
 #endif  // USE_LVGL
 
+namespace esphome{
+namespace mqtt_image{
 
 class MQTTImage : public image::Image {
 
     public:
         MQTTImage(const uint8_t *data_start, int width, int height);
 };
+
+}
+}
+
