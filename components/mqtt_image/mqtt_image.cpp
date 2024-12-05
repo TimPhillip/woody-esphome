cd6955724420378ec9226fefbad4d70bb42c76af
TimPhillip <timphillip.schneider@t-online.de>
2024-12-05 18:19:16 +0100

    added tag for logging

--- components/mqtt_image/mqtt_image.cpp
+++ components/mqtt_image/mqtt_image.cpp
@@ -16,7 +16,7 @@ image::Image(
 void MQTTImage::setup(){
 
     this->subscribe("test_topic", [this](const std::string& topic, const std::string& payload){
-        ESP_LOGW("Received Payload in MQTTImage");
+        ESP_LOGW("mqtt_image", "Received Payload in MQTTImage");
     });
 
 }
