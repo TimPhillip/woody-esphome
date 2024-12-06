79f582b402cb710f0acec2ac44259fdbb09a73a3
TimPhillip <timphillip.schneider@t-online.de>
2024-12-06 11:17:03 +0100

    log to DEBUG

--- components/mqtt_image/mqtt_image.cpp
+++ components/mqtt_image/mqtt_image.cpp
@@ -10,17 +10,17 @@ MQTTImage::MQTTImage(const uint8_t *data_start, int width, int height):
 image::Image(
   data_start, width, height, image::ImageType::IMAGE_TYPE_RGB565
 ){
-    ESP_LOGW("mqtt_image", "MQTTImage Init");
+    ESP_LOGD("mqtt_image", "MQTTImage Init");
 }
 
 void MQTTImage::setup(){
-    ESP_LOGW("mqtt_image", "MQTTImage Setup");
+    ESP_LOGD("mqtt_image", "MQTTImage Setup");
     this->subscribe("test_topic", &MQTTImage::on_image_update);
 }
 
 void MQTTImage::on_image_update(const std::string& topic, const std::string& payload){
-    ESP_LOGW("mqtt_image", "Received Payload in MQTTImage");
+    ESP_LOGD("mqtt_image", "Received Payload in MQTTImage");
 }
 
 }
-}
+} 
